4f1de7c7f53ca6d2f12ed107d4bc322fbacb6471
J0113boy <keskitalo_6@hotmail.com>
2024-04-25 14:38:28 +0200

    fixed slight spelling error

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const app = ('express')
+const app = express()
 
 // Middleware for parsing JSON bodies
-app.use(express.json())
+app.use(express.json());
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 6000
 
 
 
