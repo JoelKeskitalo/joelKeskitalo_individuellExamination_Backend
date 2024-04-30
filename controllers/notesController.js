c4887b2cdf0aeec9d7a84a2bb0ec26387a8fdba4
J0113boy <keskitalo_6@hotmail.com>
2024-04-30 10:45:03 +0200

    minor setup for remaining controllers

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -25,13 +25,25 @@ exports.createNote = async (req, res) => {
 
 
 exports.updateNote = (req, res) => {
-    res.status(200).send({ message: 'Note updated successfully'})
+    try {
+
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to update note', error: error.message})
+    }
 }
 
 exports.deleteNote = (req, res) => {
-    res.status(200).send({ message: 'Note deleted successfully'})
+    try {
+
+    } catch(error) {
+        res.status(500).json({ message: 'Failed to delete note', error: error.message})
+    }
 }
 
 exports.searchNotes = (req, res) => {
-    res.status(200).json([{ id: '1', title: 'Sample Note', text: 'This is a sample note.', createdAt: new Date(), modifiedAt: new Date() }])
+    try {
+
+    } catch(error) {
+        res.status(500).json({ message: 'Could not find note', error: error.message})
+    }
 }
