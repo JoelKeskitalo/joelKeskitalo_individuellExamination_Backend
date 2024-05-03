eaf625241a405a9cdc1d99857ea42f240d892fbc
J0113boy <keskitalo_6@hotmail.com>
2024-05-03 12:25:44 +0200

    minor changes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,27 +1,27 @@
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken')
 
 const verifyToken = (req, res, next) => {
     // Hämta token från Authorization header
-    const authHeader = req.headers['authorization'];
+    const authHeader = req.headers['authorization']
     if (!authHeader) {
-        return res.status(403).json({ error: 'A token is required for authentication' });
+        return res.status(403).json({ error: 'A token is required for authentication' })
     }
 
     // Splitta Authorization header med mellanslag och hämta det tredje elementet
-    const token = authHeader.split(' ')[2];
+    const token = authHeader.split(' ')[2]
     console.log('Authorization Header:', authHeader);  // Logga hela headern
-    console.log('Extracted Token:', token);  // Logga extraherad token
+    console.log('Extracted Token:', token)  // Logga extraherad token
 
     // Försök verifiera token
     try {
-        const decoded = jwt.verify(token, 'secret_key');
-        req.user = decoded;
+        const decoded = jwt.verify(token, 'secret_key')
+        req.user = decoded
     } catch (err) {
-        console.log('JWT Verification Error:', err);  // Logga fel som uppstår vid verifiering
-        return res.status(401).json({ error: 'Invalid Token', details: err.message });
+        console.log('JWT Verification Error:', err)  // Logga fel som uppstår vid verifiering
+        return res.status(401).json({ error: 'Invalid Token', details: err.message })
     }
 
-    return next();
+    return next()
 };
 
-module.exports = verifyToken;
+module.exports = verifyToken
